edf8cfb43eaec165fb26e5ca9c07c37d2ec136fc
Aaron Lorenzo Silva <aaronsilva95@outlook.es>
2023-05-24 16:12:08 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  # get 'restaurants/index'
+  # get 'restaurants/show'
+  # get 'restaurants/new'
+  # get 'restaurants/edit'
+  # get 'restaurants/create'
+  # get 'restaurants/update'
+  # get 'restaurants/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
+  root to: 'application#home'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Creating 5 restaurants'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w(chinese italian japanese french belgian).sample
+  )
+  restaurant.save
+  # 5.times do
+  #   restaurant.reviews.create(
+  #     content: Faker::Movies::HarryPotter.quote,
+  #     rating: (0..5).to_a.sample
+  #   )
+  # end
+end
+puts 'Finished!'
